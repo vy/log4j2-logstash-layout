5dca03eb99acda2453a9da07ca73a6c2957a3a17
Volkan Yazici <vyazici@bol.com>
2017-05-17 14:18:21 +0200

    Small code cleanup in LogstashLayoutFatJar.

--- layout-fatjar/src/main/java/com/vlkan/log4j2/logstash/layout/LogstashLayoutFatJar.java
+++ layout-fatjar/src/main/java/com/vlkan/log4j2/logstash/layout/LogstashLayoutFatJar.java
@@ -3,7 +3,7 @@ package com.vlkan.log4j2.logstash.layout;
 /**
  * Dummy enum to generate source and javadoc JARs required by Maven Central.
  */
-public enum LogstashLayoutFatJar {;
+public enum LogstashLayoutFatJar {
 
     // Has nothing.
 
