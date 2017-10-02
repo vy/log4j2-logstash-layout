c53fc748b63981adc9d3c6b04bc96d68e5c81b19
Volkan Yazici <vyazici@bol.com>
2017-10-02 08:49:35 +0200

    Remove unused Java 8 dependency in LogstashLayoutTest.

--- layout/src/test/java/com/vlkan/log4j2/logstash/layout/LogstashLayoutTest.java
+++ layout/src/test/java/com/vlkan/log4j2/logstash/layout/LogstashLayoutTest.java
@@ -21,7 +21,6 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
